147b3bee057017807598c5db3bdcb53a9fab9cac
mmqic <mmqic@qq.com>
2021-03-26 13:41:54 +0800

    更新文件

--- preload.js
+++ preload.js
@@ -1,3 +1,5 @@
+const exec = require('child_process').exec
+
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 window.addEventListener('DOMContentLoaded', () => {
@@ -6,5 +8,15 @@ window.addEventListener('DOMContentLoaded', () => {
   const submitBtn = document.querySelector('#btn-submit')
   submitBtn.addEventListener('click', () => {
     alert(text.value)
+    const cmdStr = 'echo "rewz 中文 123"'
+
+    exec(cmdStr, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`执行的错误: ${error}`)
+        return
+      }
+      console.log(`stdout: ${stdout}`)
+      console.error(`stderr: ${stderr}`)
+    })
   })
 })
